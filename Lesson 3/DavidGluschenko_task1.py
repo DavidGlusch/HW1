440db4c12da666d0c8a33a9d56a030ba313757ff
DavidGlusch <davidglusch@gmail.com>
2022-09-14 22:45:35 +0300

    1

---
+++ Lesson 3/DavidGluschenko_task1.py
@@ -0,0 +1,3 @@
+name = 'David'
+day = 'Wednesday'
+print(f'"Good day {name}! {day} is a perfect day to learn some python."')
---
+++ Lesson 3/DavidGluschenko_task2.py
@@ -0,0 +1,3 @@
+first_name = "David"
+last_name = "Gluschenko"
+print("Greetings, " + first_name + " " + last_name + ".")
