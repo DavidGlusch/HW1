be917b93689cfe535f330fda8a6d5926a15e845a
DavidGlusch <davidglusch@gmail.com>
2022-09-28 16:45:53 +0300

    Lesson 8 homework (remade)

---
+++ Lesson 8/Task 2.py
@@ -0,0 +1,9 @@
+def make_country(country_name, capital):
+    res = {country_name: capital}
+    print(res)
+
+
+# make_country(input('Country name: '), input('Capital: '))
+make_country('Ukraine', 'Kiev')
+
+
